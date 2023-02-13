d86af7f291d80e3144e70092ac4be48300cccac2
Xavier Garrido <xavier.garrido@gmail.com>
2023-02-13 11:22:42 +0100

    Minimal working example
    - define ACT DR6 likelihood as inherited class of `LAT_MFLike`
    - define a `setup.py` to properly install the likelihood with version support
    - define a set of unit test
    - define Github actions with `yaml` linter and unit tests
    - define an example directory to run MCMC

---
+++ act_dr6_mflike/__init__.py
@@ -0,0 +1,4 @@
+from . import _version
+from .act_dr6_mflike import ACTDR6MFLike
+
+__version__ = _version.get_versions()["version"]
---
+++ act_dr6_mflike/act_dr6_mflike.py
@@ -0,0 +1,20 @@
+""".. module:: ACTDR6MFLike
+
+:Synopsis: Definition of python-native CMB likelihood for ACT DR6 data release.
+
+:Author: ACT Collaboration
+
+"""
+from mflike import MFLike
+
+
+class ACTDR6MFLike(MFLike):
+    """
+    Likelihood for ACT DR6 data release (2022)
+    """
+
+    file_base_name = "act_dr6"
+
+    _url = "https://portal.nersc.gov/cfs/sobs/users/xgarrido/act_dr6_sim"
+    _release = "v0.1"
+    install_options = {"download_url": f"{_url}/{_release}.tar.gz"}

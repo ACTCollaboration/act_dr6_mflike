393ed14c38e5c2ce321ccf7785b0b4f3fe7ca648
sgiardie <grdsrn1@unife.it>
2024-02-14 11:58:13 +0000

    renaming calibration parameters

--- act_dr6_mflike/act_dr6_mflike.py
+++ act_dr6_mflike/act_dr6_mflike.py
@@ -6,7 +6,8 @@
 
 """
 from mflike import MFLike
-
+from mflike.theoryforge import TheoryForge
+from typing import Mapping
 
 class ACTDR6MFLike(MFLike):
     """
@@ -18,3 +19,48 @@ class ACTDR6MFLike(MFLike):
     _url = "https://portal.nersc.gov/cfs/sobs/users/xgarrido/act_dr6_sim"
     _release = "v0.1"
     install_options = {"download_url": f"{_url}/{_release}.tar.gz"}
+
+    # finding correspondence between the general MFLike naming convention
+    # and specific ACT DR6 one
+    renames: dict
+
+    def initialize(self):
+        super().initialize()
+
+        self.renames = {}
+        # calE corresponds to the polarization efficiency
+        self.renames.update({f"calE_{exp}": f"p_{exp}" for exp in self.experiments}) 
+        # cal correspond to the calibration for each array
+        self.renames.update({f"cal_{exp}": f"c_{exp}" for exp in self.experiments})
+        # calG_all is the overall calibration for ACT, calibrated on Planck
+        self.renames.update({"calG_all": "cal_ACT"})
+        
+        self.expected_params_nuis = [
+                self.translate_param(par) for par in self.expected_params_nuis
+         ]
+        
+
+
+    def translate_param(self, p):
+        # Translates parameters with the above conventions
+        return self.renames.get(p, p)
+
+    def find_translation(self, p: str) -> str:
+        """
+        Checks if a parameter can be renamed, and returns the source param if so.
+        In other words, if find_translation(p) == q, then translate_param(q) = p.
+        """
+        for k, v in self.renames.items():
+            if v == p:
+                return k
+        return p
+
+
+    def loglike(self, cl, **params_values_nocosmo):
+        # Translating back params to the LAT_MFLike convention to pass them to the 
+        # inherited class
+
+        params_values_nocosmo_translated = {self.find_translation(k): params_values_nocosmo[k] 
+            for k in params_values_nocosmo.keys()}
+        
+        return super().loglike(cl, **params_values_nocosmo_translated)
--- act_dr6_mflike/tests/test_act_dr6_mflike.py
+++ act_dr6_mflike/tests/test_act_dr6_mflike.py
@@ -31,16 +31,21 @@ nuisance_params = {
     "a_psee": 0.0,
     "a_pste": 0.0,
     "xi": 0.10,
-    "calG_all": 1,
+    "beta_s": -2.5,
+    "alpha_s": 1,
+    "T_effd": 19.6,
+    "beta_d": 1.5,
+    "alpha_dT": -0.6,
+    "alpha_dE": -0.4,
+    "alpha_p": 1,
+    "cal_ACT": 1,
 }
 for pa in ["pa4_f150", "pa4_f220", "pa5_f090", "pa5_f150", "pa6_f090", "pa6_f150"]:
     nuisance_params.update(
         {
             f"bandint_shift_dr6_{pa}": 0,
-            f"cal_dr6_{pa}": 1,
-            f"calT_dr6_{pa}": 1,
-            f"calE_dr6_{pa}": 1,
-            f"alpha_dr6_{pa}": 0,
+            f"c_dr6_{pa}": 1,
+            f"p_dr6_{pa}": 1,
         }
     )
 
